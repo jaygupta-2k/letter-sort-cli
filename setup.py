56733268c6f158f2a30ccec1e345f6c3525b2b64
Jay Gupta <jaygupta.id@gmail.com>
2024-12-28 21:48:30 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,11 +12,16 @@ See the GNU General Public License for more details.
 
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="color-sort-game",
-    version="1.0.0",
+    version="1.0.1",
     author="Jay Gupta",
     description="A CLI-based color sorting game.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
@@ -25,4 +30,12 @@ setup(
     },
     install_requires=["tabulate","colorama"],
     python_requires=">=3.6",
+    project_urls={
+        "Github": "https://github.com/jaygupta-2k/color-sort-cli.git"
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
 )
